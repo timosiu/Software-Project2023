cdf65155735162d2e2ef0e0e6ca5fc4f43fbb86c
Sami Saastamoinen <sami.saastamoinen@tuni.fi>
2023-10-12 14:29:17 +0300

    update: homepage, icons & dark mode

--- frontend/src/homepage/Homepage.jsx
+++ frontend/src/homepage/Homepage.jsx
@@ -1,45 +1,50 @@
+import {
+  HomeModernIcon,
+  MagnifyingGlassIcon,
+  StarIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/solid";
+
+import { TextCardLeft, TextCardRight } from "../components/TextCard";
+
 const Homepage = () => {
   return (
     <div className="flex flex-col place-items-center bg-gradient-to-br from-sky-500 to-pink-500 h-screen">
-      <h1 className="text-8xl mt-32 text-white">Hippy Hotel</h1>
-      <h2 className="text-6xl mt-10 text-white">
-        Your Portal to Spontanious Travel
-      </h2>
-
-      <div className="grid grid-cols-2 mt-20 place-items-center">
-        <div className="flex flex-col place-items-center w-1/2">
-          <h3 className="text-4xl text-white">Last Minute Accommodations</h3>
-          <p className="text-white">
-            Find unique, affordable, and last-minute accommodations that
-            resonate with your values, whether it's a cozy cabin in the woods or
-            an artistic urban loft.
-          </p>
-        </div>
-        <div></div>
-        <div></div>
-        <div className="flex flex-col place-items-center w-1/2">
-          <h3 className="text-4xl text-white">Community Vibes</h3>
-          <p className="text-white">
-            Connect with like-minded travelers and hosts who share your passion
-            for authentic, off-the-beaten-path experiences.
-          </p>
-        </div>
-        <div className="flex flex-col place-items-center w-1/2">
-          <h3 className="text-4xl text-white">Profiles & Reviews</h3>
-          <p className="text-white">
-            Detailed user and company profiles with ratings and reviews,
-            ensuring transparency and trust among our community members.
-          </p>
-        </div>
-        <div></div>
-        <div></div>
-        <div className="flex flex-col place-items-center w-1/2">
-          <h3 className="text-4xl text-white">Search & Book</h3>
-          <p className="text-white">
-            Our user-friendly search feature lets you discover hidden gems in
-            non-touristy areas, even at the eleventh hour.
-          </p>
-        </div>
+      <div className="flex flex-col place-items-center h-1/2 mx-10">
+        <h1 className="text-8xl mt-32 text-white text-center">Hippy Hotel</h1>
+        <h2 className="text-6xl mt-10 text-white text-center">
+          Your Portal to Spontanious Travel
+        </h2>
+      </div>
+      <div className="grid grid-cols-3 gap-y-32 gap-x-10 mt-20 place-items-center bg-white dark:bg-neutral-800 rounded-t-3xl w-full p-10 pt-20">
+        <TextCardLeft
+          title={"Last Minute Accommodations"}
+          text={
+            "Find unique, affordable, and last-minute accommodations that resonate with your values, whether it's a cozy cabin in the woods or an artistic urban loft."
+          }
+          Img={HomeModernIcon}
+        />
+        <TextCardRight
+          title={"Community Vibes"}
+          text={
+            "Connect with like-minded travelers and hosts who share your passion for authentic, off-the-beaten-path experiences."
+          }
+          Img={UserGroupIcon}
+        />
+        <TextCardLeft
+          title={"Profiles & Reviews"}
+          text={
+            "Detailed user and company profiles with ratings and reviews, ensuring transparency and trust among our community members."
+          }
+          Img={StarIcon}
+        />
+        <TextCardRight
+          title={"Search & Book"}
+          text={
+            "Our user-friendly search feature lets you discover hidden gems in non-touristy areas, even at the eleventh hour."
+          }
+          Img={MagnifyingGlassIcon}
+        />
       </div>
     </div>
   );
