344d290790b17d1bb3693a8aba46bd030098160e
Aleksi Virtanen <avek91@hotmail.com>
2023-10-23 17:43:39 +0300

    backend getLocationById

--- backend/routes/locations.js
+++ backend/routes/locations.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getLocations } = require("../controllers/locations");
+const { getLocations, getLocationById } = require("../controllers/locations");
 
 router.get("/", getLocations);
+router.get("/:id", getLocationById);
 
 module.exports = router;
