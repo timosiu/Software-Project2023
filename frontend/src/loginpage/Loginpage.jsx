954b7b6acec9dd29d5c8e3fb7142fab948281a6f
William Reima <william.reima@gmail.com>
2023-10-30 22:52:18 +0200

    working loading spinner with blurring background when logging/registering

--- frontend/src/loginpage/Loginpage.jsx
+++ frontend/src/loginpage/Loginpage.jsx
@@ -12,6 +12,7 @@ const Loginpage = () => {
   // Authentication
   const auth = useContext(AuthContext);
   const [authError, setAuthError] = useState(false);
+  const [isLoading, setisLoading] = useState(false);
 
   const nameRef = useRef();
   const emailRef = useRef();
@@ -26,6 +27,7 @@ const Loginpage = () => {
     onError: (error) => {
       console.log(error);
       setAuthError("Failed to sign up, please try again");
+      setisLoading(false);
     },
   });
 
@@ -38,12 +40,14 @@ const Loginpage = () => {
     onError: (error) => {
       console.log(error);
       setAuthError("failed to log in, please check your credentials");
+      setisLoading(false);
     },
   });
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
     setAuthError(false);
+    setisLoading(true);
 
     if (tab) {
       loginUserMutation.mutate({
@@ -58,6 +62,19 @@ const Loginpage = () => {
       });
     }
   };
+
+  const loadingSpinner = (
+    <div className="absolute inset-0 flex items-center justify-center backdrop-blur">
+      <div
+        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading ...
+        </span>
+      </div>
+    </div>
+  );
 
   let content;
 
@@ -206,6 +223,7 @@ const Loginpage = () => {
       </div>
       <div class="max-w-md px-10 bg-gray-50 dark:bg-neutral-800 rounded-lg ">
         {content}
+        {isLoading && loadingSpinner}
       </div>
     </div>
   );
