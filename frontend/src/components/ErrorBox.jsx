cf7d089ad3ff2b14546ddfb789cee1c34eb40a9c
William Reima <william.reima@gmail.com>
2023-11-09 18:27:22 +0200

    cleanup

--- frontend/src/components/ErrorBox.jsx
+++ frontend/src/components/ErrorBox.jsx
@@ -1,11 +1,10 @@
 const ErrorBox = (props) => {
-  const errorText = props.errorText;
   return (
     <div
       className=" font-bold space-y-8 md:space-y-10 mt-6 px-2 py-2 bg-red-500 text-white rounded mb-0
     max-w-auto whitespace-normal overflow-hidden text-ellipsis"
     >
-      {errorText}
+      {props.errorText}
     </div>
   );
 };
