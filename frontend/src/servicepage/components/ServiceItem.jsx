5e827bf406420fdb2a5d3c07e82e7ebc98ce0a2b
Sami Saastamoinen <sami.saastamoinen@tuni.fi>
2023-11-26 17:38:35 +0200

    Remove rounding from photos

--- frontend/src/servicepage/components/ServiceItem.jsx
+++ frontend/src/servicepage/components/ServiceItem.jsx
@@ -5,7 +5,7 @@ const ServiceItem = ({ service }) => {
     <div className="flex flex-col md:flex-row col-span-3 w-full mb-10 px-10">
       <img
         src={serviceImages[0]}
-        className="aspect-video max-w-md md:max-w-l md:w-full h-60 md:h-96 object-cover rounded-lg mt-10"
+        className="aspect-video max-w-md md:max-w-l md:w-full h-60 md:h-96 object-cover mt-10"
       />
       <div className="flex flex-col w-full mt-6 md:mt-10 ml-0 md:ml-6">
         <h3 className="text-4xl text-light-text dark:text-dark-text">
