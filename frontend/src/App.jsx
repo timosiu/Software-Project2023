43d51b0d058ce6e8277e1ed09af302db25b385db
Sami Saastamoinen <sami.saastamoinen@tuni.fi>
2023-11-27 17:21:55 +0200

    Add links to pages & Rename Sustain... -> Values

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ import Homepage from "./homepage/Homepage";
 import Loginpage from "./loginpage/Loginpage";
 import Contactpage from "./contactpage/Contactpage";
 import ServiceListpage from "./servicepage/ServiceListpage";
-import Sustainabilitypage from "./sustainabilitypage/Sustainabilitypage";
+import Valuepage from "./valuepage/Valuepage";
 import Profilepage from "./profilepage/Profilepage";
 import ReservationPage from "./reservationpage/ReservationPage";
 import Navbar from "./navbar/Navbar";
@@ -85,7 +85,7 @@ function App() {
       <Route path="/loginpage" element={<Loginpage />} />
       <Route path="/contactpage" element={<Contactpage />} />
       <Route path="/servicepage" element={<ServiceListpage />} />
-      <Route path="/sustainabilitypage" element={<Sustainabilitypage />} />
+      <Route path="/valuepage" element={<Valuepage />} />
       <Route path="/profile" element={<Profilepage />} />
       <Route path="/reservation" element={<ReservationPage />} />
       <Route path="*" element={<Navigate to="/" />} />
